30b6369c08510f2f8b4e3e03baf146e8ac303af1
matvei <matveikylosov@gmail.com>
2025-03-26 23:55:51 +0500

    Сделал главное окно, страничку расходников и добавил затычки

---
+++ UP02/Pages/Main/MainPage.xaml.cs
@@ -0,0 +1,318 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+using UP02.Context;
+using UP02.DocumentProcessing;
+using UP02.Helpers;
+using UP02.Models;
+
+namespace UP02.Pages.Main
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        // Определяем таблицы, с которыми будем работать
+        private enum Tables
+        {
+            Directions,
+            EquipmentModels,
+            Statuses,
+            TypesConsumables,
+            SoftwareDevelopers,
+            Users,
+            Audiences,
+            ConsumableCharacteristics,
+            ConsumableCharacteristicValues,
+            Equipment,
+            Consumables,
+            EquipmentConsumables,
+            Software,
+            Errors,
+            Inventories,
+            EquipmentLocationHistory,
+            EquipmentResponsibleHistory,
+            InventoryChecks,
+            NetworkSettings
+        }
+
+        private Tables ActiveTable;
+
+        private readonly Dictionary<Tables, Action> tableHandlers;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Инициализация словаря обработчиков таблиц
+            tableHandlers = new Dictionary<Tables, Action>
+            {
+                { Tables.Audiences, LoadAudiences },
+                { Tables.Directions, LoadDirections },
+                { Tables.EquipmentModels, LoadEquipmentModels },
+                { Tables.SoftwareDevelopers, LoadSoftwareDevelopers },
+                { Tables.Users, LoadUsers },
+                { Tables.Software, LoadSoftware },
+                { Tables.NetworkSettings, LoadNetworkSettings },
+                { Tables.Consumables, LoadConsumables},
+                { Tables.TypesConsumables, LoadTypesConsumables },
+                { Tables.Equipment, LoadEquipment },
+                { Tables.EquipmentConsumables, LoadEquipmentConsumables},
+                { Tables.Inventories, LoadInventories },
+                { Tables.Statuses, LoadStatuses },
+            };
+        }
+
+        /// <summary>
+        /// Универсальный метод загрузки данных для выбранной таблицы.
+        /// </summary>
+        /// <param name="table">Выбранная таблица.</param>
+        private void LoadTable(Tables table)
+        {
+            ActiveTable = table;
+            MainContentPanel.Children.Clear();
+
+            if (tableHandlers.TryGetValue(table, out var handler))
+            {
+                handler.Invoke();
+            }
+            else
+            {
+                MessageBox.Show("Обработчик для выбранной таблицы не найден.");
+            }
+        }
+
+        /// <summary>
+        /// Загрузка аудитории с БД.
+        /// </summary>
+        /// 
+
+        private void LoadAudiences()
+        {
+            ContentFrame.Navigate(new PageAudiences());
+        }
+
+        /// <summary>
+        /// Загрузка направлений.
+        /// </summary>
+        private void LoadDirections()
+        {
+            ContentFrame.Navigate(new PageDirections());
+        }
+
+        /// <summary>
+        /// Загрузка моделей оборудования.
+        /// </summary>
+        private void LoadEquipmentModels()
+        {
+            ContentFrame.Navigate(new PageEquipmentModels());
+        }
+
+        /// <summary>
+        /// Загрузка разработчиков ПО.
+        /// </summary>
+        private void LoadSoftwareDevelopers()
+        {
+            ContentFrame.Navigate (new PageSoftwareDevelopers());
+        }
+
+        /// <summary>
+        /// Загрузка пользователей.
+        /// </summary>
+        private void LoadUsers()
+        {
+            ContentFrame.Navigate(new PageUsers());
+        }
+
+        /// <summary>
+        /// Загрузка ПО.
+        /// </summary>
+        private void LoadSoftware()
+        {
+            ContentFrame.Navigate(new PageSoftware());
+        }
+
+        /// <summary>
+        /// Загрузка сетевых настроек.
+        /// </summary>
+        private void LoadNetworkSettings()
+        {
+            ContentFrame.Navigate(new PageNetworkSettings());
+        }
+
+        private void LoadConsumables()
+        {
+            ContentFrame.Navigate(new PageConsumables());
+        }
+
+        private void LoadTypesConsumables()
+        {
+            ContentFrame.Navigate(new PageTypesConsumables());
+        }
+
+        private void LoadEquipment()
+        {
+            ContentFrame.Navigate (new PageEquipment());
+        }
+
+        private void LoadStatuses() 
+        {
+            ContentFrame.Navigate(new PageStatuses());
+        }
+
+        private void LoadEquipmentConsumables()
+        {
+            ContentFrame.Navigate(new PageEquipmentConsumables());
+        }
+
+        private void LoadInventories()
+        {
+            ContentFrame.Navigate(new PageInventories());
+        }
+
+        private void AudiencesButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.Audiences);
+
+        private void DirectionsButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.Directions);
+
+        private void EquipmentModelsButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.EquipmentModels);
+
+        private void SoftwareDevelopersButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.SoftwareDevelopers);
+        private void UsersButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.Users);
+
+        private void SoftwareButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.Software);
+
+        private void NetworkSettingsButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.NetworkSettings);
+
+        private void ConsumablesButton_Click(object sender, RoutedEventArgs e) 
+            => LoadTable(Tables.Consumables);
+
+        private void TypesConsumablesButton_Click(object sender, RoutedEventArgs e) 
+            => LoadTable(Tables.TypesConsumables);
+
+        private void EquipmentButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.Equipment);
+
+        private void EquipmentConsumablesButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.EquipmentConsumables);
+
+        private void InventoriesButton_Click(object sender, RoutedEventArgs e)
+            => LoadTable(Tables.Inventories);
+
+        private void StatusesButton_Click(object sender, RoutedEventArgs e) =>
+            LoadTable(Tables.Statuses);
+
+
+        /// <summary>
+        /// Обработчик для импорта данных из файла Excel.
+        /// </summary>
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel Files|*.xls;*.xlsx",
+                Title = "Выберите файл Excel",
+                Multiselect = false // Разрешаем выбрать только один файл
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFile = openFileDialog.FileName;
+                var excelDataImporter = new ExcelDataImporter(selectedFile);
+                (string error, List<UserImport> users) = excelDataImporter.ReadData();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show(error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                using var databaseContext = new DatabaseContext();
+
+                try
+                {
+                    // Выгружаем список существующих пользователей в память для быстрой проверки
+                    var existingUserNames = databaseContext.Users
+                        .AsNoTracking()
+                        .ToList() // Необходим для клиентской фильтрации
+                        .Select(u => u.FullName)
+                        .ToHashSet();
+
+                    var notExistingUser = users.FirstOrDefault(x => !existingUserNames.Contains(x.FullName));
+                    if (notExistingUser != null)
+                    {
+                        MessageBox.Show($"Пользователь {notExistingUser.FullName} не существует.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+
+                    // Для каждого импортированного пользователя
+                    foreach (var user in users)
+                    {
+                        // Выгружаем всех пользователей в память и ищем пользователя по полному имени
+                        var allUsers = databaseContext.Users.AsNoTracking().ToList();
+                        var databaseUser = allUsers.FirstOrDefault(x => x.FullName == user.FullName);
+                        if (databaseUser == null)
+                        {
+                            MessageBox.Show($"Пользователь {user.FullName} не существует.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+                        // Прикрепляем найденного пользователя к контексту
+                        databaseContext.Attach(databaseUser);
+
+                        // Обработка направлений для пользователя
+                        foreach (var direction in user.Directions)
+                        {
+                            // Ищем направление по имени
+                            var databaseDirection = databaseContext.Directions.FirstOrDefault(x => x.Name == direction.DirectionName);
+                            if (databaseDirection == null)
+                            {
+                                // Если направление не найдено, создаём новое
+                                databaseDirection = new Directions { Name = direction.DirectionName };
+                                databaseContext.Directions.Add(databaseDirection);
+                                databaseContext.SaveChanges();
+                            }
+                            else
+                            {
+                                databaseContext.Attach(databaseDirection);
+                            }
+
+                            // Обработка оборудования для каждого направления
+                            foreach (var equipment in direction.Equipments)
+                            {
+                                for (int i = 0; i < equipment.Quantity; i++)
+                                {
+                                    var equipmentForDatabase = new Equipment
+                                    {
+                                        Name = equipment.Name,
+                                        DirectionID = databaseDirection.DirectionID,
+                                        Direction = databaseDirection,
+                                        InventoryNumber = equipment.InventoryNumber,
+                                        ResponsibleUserID = databaseUser.UserID,
+                                        ResponsibleUser = databaseUser,
+                                    };
+                                    databaseContext.Equipment.Add(equipmentForDatabase);
+                                }
+                            }
+                        }
+                        databaseContext.SaveChanges();
+                    }
+
+                    MessageBox.Show($"Данные успешно импортированы.", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    UIHelper.ErrorConnection(databaseContext, ex.Message);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ UP02/Pages/Main/PageConsumables.xaml.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UP02.Context;
+using UP02.Elements;
+using UP02.Helpers;
+using UP02.Models;
+using UP02.Pages.Elements;
+
+namespace UP02.Pages.Main
+{
+    /// <summary>
+    /// Логика взаимодействия для PageConsumables.xaml
+    /// </summary>
+    public partial class PageConsumables : Page
+    {
+        List<Consumables> OriginalRecords = new List<Consumables>();
+        List<Consumables> CurrentList = new List<Consumables>();
+
+        /// <summary>
+        /// Конструктор страницы, инициализирует компоненты, загружает данные из базы и настраивает элементы управления.
+        /// </summary>
+        public PageConsumables()
+        {
+            InitializeComponent();
+
+            using var databaseContext = new DatabaseContext();
+            try
+            {
+                OriginalRecords = databaseContext.Consumables.Include(a => a.TempResponsibleUser)
+                            .Include(a => a.TypeConsumables)
+                            .ToList();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось подключиться к базе данных. Проверьте соединение и повторите попытку.",
+                                "Ошибка подключения", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                MainWindow.OpenPage(new PageAuthorization());
+                return;
+            }
+
+            List<Users> users = OriginalRecords.Where(u => u != null && u.TempResponsibleUser != null).Select(e => e.TempResponsibleUser).Distinct().ToList();
+            List<TypesConsumables> types = OriginalRecords.Where(u => u != null && u.TypeConsumables != null).Select(e => e.TypeConsumables).Distinct().ToList();
+
+            users.Insert(0, new Users { UserID = -1, LastName = "", FirstName = "Отсутствует", MiddleName = "" });
+            types.Insert(0, new TypesConsumables { TypeConsumablesID = -1, Type = "Отсутствует" });
+
+            TempResponsibleUserCB.ItemsSource = users;
+            TempResponsibleUserCB.DisplayMemberPath = "FullName";
+            TempResponsibleUserCB.SelectedValuePath = "UserID";
+
+            TypeConsumablesCB.ItemsSource = types;
+            TypeConsumablesCB.DisplayMemberPath = "Type";
+            TypeConsumablesCB.SelectedValuePath = "TypeConsumablesID";
+
+            TempResponsibleUserCB.SelectedValue = -1;
+            TypeConsumablesCB.SelectedValue = -1;
+
+            CurrentList = OriginalRecords;
+
+            UIHelper.AddItemsToPanel(ContentPanel, CurrentList, x => new ItemConsumables(x), OriginalRecords);
+        }
+
+        /// <summary>
+        /// Обработчик клика по кнопке для добавления нового расходного материала.
+        /// </summary>
+        private void AddNewRecord_Click(object sender, RoutedEventArgs e)
+        {
+            var editPage = new EditConsumables();
+            editPage.RecordSuccess += CreateNewRecordSuccess;
+            MainWindow.mainFrame.Navigate(editPage);
+        }
+
+        /// <summary>
+        /// Обработчик успешного создания нового расходного материала, добавляет запись в список и сортирует данные.
+        /// </summary>
+
+        private void CreateNewRecordSuccess(object sender, EventArgs e)
+        {
+            var consumable = sender as Consumables;
+            if (consumable == null)
+                return;
+
+            OriginalRecords.Add(consumable);
+            SortRecord();
+        }
+
+        /// <summary>
+        /// Метод для сортировки и фильтрации записей на основе выбранных значений и поискового запроса.
+        /// </summary>
+        private void SortRecord()
+        {
+            CurrentList = OriginalRecords;
+
+            int? selectedTempResponsible = TempResponsibleUserCB.SelectedValue as int?;
+            if (selectedTempResponsible.HasValue && selectedTempResponsible.Value != -1)
+            {
+                CurrentList = CurrentList.Where(x => x.TempResponsibleUserID == selectedTempResponsible.Value).ToList();
+            }
+
+            int? selectedTypeConsumablesCB = TypeConsumablesCB.SelectedValue as int?;
+            if (selectedTypeConsumablesCB.HasValue && selectedTypeConsumablesCB.Value != -1)
+            {
+                CurrentList = CurrentList.Where(x => x.TypeConsumablesID == selectedTypeConsumablesCB.Value).ToList();
+            }
+
+            string searchQuery = SearchField.Text.Trim();
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                CurrentList = CurrentList
+                    .Where(x => x.Name.IndexOf(searchQuery, StringComparison.CurrentCultureIgnoreCase) >= 0 ||
+                                (x.Description != null && x.Description.IndexOf(searchQuery, StringComparison.CurrentCultureIgnoreCase) >= 0) ||
+                                (x.TempResponsibleUser != null && x.TempResponsibleUser.FullName.IndexOf(searchQuery, StringComparison.CurrentCultureIgnoreCase) >= 0) ||
+                                (x.TypeConsumables != null && x.TypeConsumables.Type.IndexOf(searchQuery, StringComparison.CurrentCultureIgnoreCase) >= 0)
+                          )
+                    .ToList();
+            }
+
+            if (AfterReceiptDate.SelectedDate.HasValue)
+            {
+                DateTime afterReceiptDate = AfterReceiptDate.SelectedDate.Value;
+                CurrentList = CurrentList.Where(x => x.ReceiptDate >= afterReceiptDate).ToList();
+            }
+
+            if (BeforeReceiptDate.SelectedDate.HasValue)
+            {
+                DateTime beforeReceiptDate = BeforeReceiptDate.SelectedDate.Value;
+                CurrentList = CurrentList.Where(x => x.ReceiptDate <= beforeReceiptDate).ToList();
+            }
+
+            ContentPanel.Children.Clear();
+            UIHelper.AddItemsToPanel(ContentPanel, CurrentList, x => new ItemConsumables(x), OriginalRecords);
+        }
+
+        /// <summary>
+        /// Обработчик изменения значения в комбинированном списке сортировки.
+        /// </summary>
+        private void SortCB_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            SortRecord();
+        }
+
+        /// <summary>
+        /// Обработчик клика по кнопке поиска, выполняет сортировку данных.
+        /// </summary>
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            SortRecord();
+        }
+
+        /// <summary>
+        /// Обработчик изменения даты в поле выбора даты получения, выполняет сортировку.
+        /// </summary>
+        private void ReceiptDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SortRecord();
+        }
+
+        /// <summary>
+        /// Обработчик ввода текста в поле выбора даты получения, выполняет сортировку.
+        /// </summary>
+        private void ReceiptDate_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            SortRecord();
+        }
+
+        /// <summary>
+        /// Обработчик потери фокуса в поле выбора даты получения, выполняет сортировку.
+        /// </summary>
+        private void ReceiptDate_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SortRecord();
+        }
+    }
+}
