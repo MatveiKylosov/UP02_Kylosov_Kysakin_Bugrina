932c6cc4e08e6858b4587fe922da39b34211742b
HP <HP@DESKTOP-T6A34MC>
2025-03-27 01:33:50 +0500

    Главная страница типов расходников. Бугрина

--- UP02/Pages/Main/PageTypesConsumables.xaml.cs
+++ UP02/Pages/Main/PageTypesConsumables.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UP02.Context;
+using UP02.Helpers;
+using UP02.Models;
+using UP02.Pages.Elements;
 
 namespace UP02.Pages.Main
 {
@@ -20,9 +24,80 @@ namespace UP02.Pages.Main
     /// </summary>
     public partial class PageTypesConsumables : Page
     {
+        List<TypesConsumables> OriginalRecords = new List<TypesConsumables>();
+        List<TypesConsumables> CurrentList = new List<TypesConsumables>();
+
+        /// <summary>
+        /// Конструктор страницы типов расходных материалов. Инициализирует компоненты и загружает данные из базы данных.
+        /// </summary>
         public PageTypesConsumables()
         {
             InitializeComponent();
+            try
+            {
+                using var databaseContext = new DatabaseContext();
+                OriginalRecords = databaseContext.TypesConsumables.ToList();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось подключиться к базе данных. Проверьте соединение и повторите попытку.",
+                                "Ошибка подключения", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                MainWindow.OpenPage(new PageAuthorization());
+                return;
+            }
+
+            UIHelper.AddItemsToPanel(ContentPanel, OriginalRecords, x => new ItemTypesConsumables(x), OriginalRecords);
+        }
+
+        /// <summary>
+        /// Обработчик клика по кнопке "Добавить новую запись". Открывает страницу для редактирования новой записи типа расходного материала.
+        /// </summary>
+        private void AddNewRecord_Click(object sender, RoutedEventArgs e)
+        {
+            var editPage = new EditTypesConsumables();
+            editPage.RecordSuccess += CreateNewRecordSuccess;
+            MainWindow.mainFrame.Navigate(editPage);
+        }
+
+        /// <summary>
+        /// Обработчик успешного создания новой записи типа расходного материала. Добавляет запись в оригинальный список и сортирует записи.
+        /// </summary>
+        private void CreateNewRecordSuccess(object sender, EventArgs e)
+        {
+            var direction = sender as TypesConsumables;
+            if (direction == null)
+                return;
+
+            OriginalRecords.Add(direction);
+            SortRecord();
+        }
+
+        /// <summary>
+        /// Метод сортировки записей типов расходных материалов. Применяет фильтры по поисковому запросу, обновляя отображаемые записи.
+        /// </summary>
+        private void SortRecord()
+        {
+            CurrentList = OriginalRecords;
+
+            string searchQuery = SearchField.Text.Trim();
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                CurrentList = CurrentList
+                    .Where(x => x.Type.IndexOf(searchQuery, StringComparison.CurrentCultureIgnoreCase) >= 0)
+                    .ToList();
+            }
+
+            ContentPanel.Children.Clear();
+            UIHelper.AddItemsToPanel(ContentPanel, CurrentList, x => new ItemTypesConsumables(x), OriginalRecords);
+        }
+
+        /// <summary>
+        /// Обработчик клика по кнопке "Поиск". Перезапускает сортировку с применением фильтра по поисковому запросу.
+        /// </summary>
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            SortRecord();
         }
     }
 }
