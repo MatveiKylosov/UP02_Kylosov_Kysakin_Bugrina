95a8ba140d63df572eca967c5f5932184b8edf04
acidgost <ac1dghost@yandex.ru>
2025-03-27 13:16:46 +0500

    Сделан код для страниц программ и разработчиков программ Кусакин

--- UP02/Pages/Main/PageSoftwareDevelopers.xaml.cs
+++ UP02/Pages/Main/PageSoftwareDevelopers.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UP02.Context;
+using UP02.Elements;
+using UP02.Helpers;
+using UP02.Models;
+using UP02.Pages.Elements;
 
 namespace UP02.Pages.Main
 {
@@ -20,9 +13,83 @@ namespace UP02.Pages.Main
     /// </summary>
     public partial class PageSoftwareDevelopers : Page
     {
+        List<SoftwareDevelopers> OriginalRecords = new List<SoftwareDevelopers>();
+        List<SoftwareDevelopers> CurrentList = new List<SoftwareDevelopers>();
+
+        /// <summary>
+        /// Конструктор страницы разработчиков программного обеспечения. Инициализирует компоненты и загружает данные о разработчиках 
+        /// программного обеспечения из базы данных.
+        /// </summary>
         public PageSoftwareDevelopers()
         {
             InitializeComponent();
+            try
+            {
+                using var databaseContext = new DatabaseContext();
+                OriginalRecords = databaseContext.SoftwareDevelopers.ToList();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось подключиться к базе данных. Проверьте соединение и повторите попытку.",
+                                "Ошибка подключения", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                MainWindow.OpenPage(new PageAuthorization());
+                return;
+            }
+            CurrentList = OriginalRecords;
+            UIHelper.AddItemsToPanel(ContentPanel, CurrentList, x => new ItemSoftwareDevelopers(x), OriginalRecords);
+        }
+
+
+        /// <summary>
+        /// Обработчик клика по кнопке "Добавить новую запись". Открывает страницу для редактирования новой записи разработчика программного обеспечения.
+        /// </summary>
+        private void AddNewRecord_Click(object sender, RoutedEventArgs e)
+        {
+            var editPage = new EditSoftwareDevelopers();
+            editPage.RecordSuccess += CreateNewRecordSuccess;
+            MainWindow.mainFrame.Navigate(editPage);
+        }
+
+        /// <summary>
+        /// Обработчик успешного создания новой записи разработчика программного обеспечения. Добавляет запись в оригинальный список и сортирует записи.
+        /// </summary>
+        private void CreateNewRecordSuccess(object sender, EventArgs e)
+        {
+            var SoftwareDevelopers = sender as SoftwareDevelopers;
+            if (SoftwareDevelopers == null)
+                return;
+
+            OriginalRecords.Add(SoftwareDevelopers);
+            SortRecord();
+        }
+
+
+        /// <summary>
+        /// Метод сортировки записей. Применяет фильтры по поисковому запросу, обновляя отображаемые записи.
+        /// </summary>
+        private void SortRecord()
+        {
+            CurrentList = OriginalRecords;
+
+            string searchQuery = SearchField.Text.Trim();
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                CurrentList = CurrentList
+                    .Where(x => x.Name.IndexOf(searchQuery, StringComparison.CurrentCultureIgnoreCase) >= 0)
+                    .ToList();
+            }
+
+            ContentPanel.Children.Clear();
+            UIHelper.AddItemsToPanel(ContentPanel, CurrentList, x => new ItemSoftwareDevelopers(x), OriginalRecords);
+        }
+
+        /// <summary>
+        /// Обработчик клика по кнопке "Поиск". Перезапускает сортировку с применением фильтра по поисковому запросу.
+        /// </summary>
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            SortRecord();
         }
     }
 }
