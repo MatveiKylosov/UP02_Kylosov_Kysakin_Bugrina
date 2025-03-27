97b433ecae5786388c1f6d2b913f67ca1f7fce94
acidgost <ac1dghost@yandex.ru>
2025-03-27 13:41:52 +0500

    Сделана страница авторизации Кусакин

--- UP02/MainWindow.xaml.cs
+++ UP02/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UP02.Pages.Main;
 
 namespace UP02;
 
@@ -21,6 +14,7 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         mainFrame = this.MainFrame;
+        OpenPage(new PageAuthorization());
     }
 
     public static void ClearFrame()
